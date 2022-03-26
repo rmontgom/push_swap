6960498e3aae9942555698597db1a3768b7a9595
Ronda Montgomery <rmontgom@ev-o1.kzn.21-school.ru>
2022-03-26 17:23:57 +0300

    first commit

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,13 @@
 /*   By: rmontgom <rmontgom@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 21:59:33 by rmontgom          #+#    #+#             */
-/*   Updated: 2022/03/26 03:44:00 by rmontgom         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:20:23 by rmontgom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "push_swap.h"
 
-#include "libft.h"
+#include "./libft/libft.h"
 #include <stdio.h>
 
 void	free_pointer(char **ptr)
@@ -26,6 +26,40 @@ void	free_pointer(char **ptr)
 		i++;
 	}
 	free(ptr);
+}
+
+int	duplicat(char **arr)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (arr[i])
+	{
+		j = i + 1;
+		while (arr[j])
+		{
+			if (ft_atoi(arr[i]) == ft_atoi(arr[j]))
+			{
+				free_pointer(arr);
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+// typedef struct s_list
+// {
+// 	int value;
+// 	int index;
+// }; t_list
+
+void Error()
+{
+	write(2, "Error\n", 6);
 }
 
 int	get_size(char **argv)
@@ -53,27 +87,33 @@ int	*pars_elements(char **argv)
 {
 	int	i;
 	char	**buf;
-	int *numb;
+	int *unsort;
 	int k;
 	int j;
 
-	numb = (int *)malloc(sizeof(int) * (get_size(argv)));
+	unsort = (int *)malloc(sizeof(int) * (get_size(argv)));
+	if (!unsort)
+		return (NULL);
 	i = 1;
 	while (argv[i])
 	{
 		buf = ft_split(argv[i], ' ');
 		k = 0;
-		j = 0;	
-		while (buf[j])
+		j = 0;
+		if (duplicat(buf) == 1)
+			Error();
+		else
 		{
-			numb[k] = ft_atoi(buf[j++]);
-			// printf("%d\n", numb[k]);
-			k++;
-		}	
-		// free_pointer(buf);
-		i++;
+			while (buf[j])
+			{
+				unsort[k++] = ft_atoi(buf[j++]);
+				printf("%d\n", unsort[k]);
+			}
+			free_pointer(buf);
+			i++;
+		}
 	}
-	return (numb);
+	return (unsort);
 }
 // 	i = 1;
 // 	*buf = (char *)malloc(sizeof(char) * argc)
@@ -91,13 +131,12 @@ int main(int argc, char **argv)
 	int i;
 	
 	p = get_size(argv);
-	// printf("%d\n", p);
-	s = pars_elements(argv);
-	i = 0;
-	while (i < p)
-	{
-		printf("%d\n", pars_elements(argv)[i]);
-		i++;
-	}
-	return (0);	
+	printf("%d\n", p);
+	pars_elements(argv);
+	// while (i < p)
+	// {
+	// 	printf("%d\n", pars_elements(argv)[i]);
+	// 	i++;
+	// }
+	// return (0);	
 }
